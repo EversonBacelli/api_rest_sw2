e992c7ba6680a8dfb268a00e62f761410f3ca734
Everson Bacelli <42649430+EversonBacelli@users.noreply.github.com>
2024-06-08 11:04:35 -0300

    Update escrever.php

--- controller/escrever.php
+++ controller/escrever.php
@@ -1,7 +1,7 @@
 <?php
     function escrever($json){
         // endereço do arquivo
-        $arquivo = "https://apirestsw2-production.up.railway.app/controller/produtos.txt";
+        $arquivo = "./produtos.txt";
 
         //Variável $fp armazena a conexão com o arquivo e o tipo de ação.
         $fp = fopen($arquivo, "w+");
