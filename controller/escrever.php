60be4442bffba118e8dec1cd277dfe9077cdf7a0
Everson Bacelli <42649430+EversonBacelli@users.noreply.github.com>
2024-06-08 10:49:09 -0300

    Update escrever.php com o endereço do railway

--- controller/escrever.php
+++ controller/escrever.php
@@ -1,7 +1,7 @@
 <?php
     function escrever($json){
         // endereço do arquivo
-        $arquivo = "./produtos.txt";
+        $arquivo = "https://apirestsw2-production.up.railway.app/controller/produtos.txt";
 
         //Variável $fp armazena a conexão com o arquivo e o tipo de ação.
         $fp = fopen($arquivo, "w+");
